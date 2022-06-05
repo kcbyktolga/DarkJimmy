2b836ed7372d5e9da25b0b49dc773dc71a2e79a3
Tolga Kocabıyık <tolgakcbyk@outlook.com>
2022-06-06 00:52:53 +0300

    + Created Level Data

---
+++ Assets/Scripts/ScriptableObjects/LevelData.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using DarkJimmy.UI;
+
+namespace DarkJimmy
+{
+    public class LevelData : ScriptableObject
+    {
+        [Header("Property")]
+        [SerializeField]
+        private Sprite passed;
+        [SerializeField]
+        private Sprite active;
+        [SerializeField]
+        private Sprite passive;
+
+        public LevelTab levelTab;
+
+        [Header("Stages")]
+        public List<Stage> stages;    
+        
+        public Sprite GetLevelSprite(LevelStatus status)
+        {
+            return status switch
+            {
+                LevelStatus.Active => active,
+                LevelStatus.Passive => passive,
+                _ => passed,
+            };
+        }
+        
+
+    }
+
+    [Serializable]
+    public class Stage
+    {
+        public string stageName;
+        public bool stageIsLocked=true;
+        public Sprite stageIcon;
+        public List<Level> levels;
+    }
+
+    [Serializable]
+    public class Level
+    {
+        public LevelStatus levelStatus;
+        public int rankCount;
+
+    }
+    public enum LevelStatus
+    {
+        Passed,
+        Active,
+        Passive
+    }
+}
+
